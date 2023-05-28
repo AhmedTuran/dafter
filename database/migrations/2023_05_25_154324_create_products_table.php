05a2584d1c8d942b531106ee2f17d6af2cbc2464
SWENG Ahmed K. Turban <74715701+AhmedTuran@users.noreply.github.com>
2023-05-28 17:30:20 +0300

    my farset step => get the app

---
+++ database/migrations/2023_05_25_154324_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('photo');
+            $table->double('price');
+            $table->double('offerPrice')->nullable();
+            $table->double('unitNumber');//unit number on storeg
+            $table->unsignedBigInteger('catigories_id');
+            $table->date('expiryDate')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
